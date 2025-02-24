5b8822c5f21025260668d614a366f4cb5e944acc
Matheus Feijó <mat.feijo2506@gmail.com>
2025-02-24 11:28:51 -0400

    FEAT/cardItem: Criando testes de redirecionar para tela de detalhes e valor do produto formatado

--- src/components/card-item.spec.tsx
+++ src/components/card-item.spec.tsx
@@ -1,19 +1,13 @@
 import { IProduct } from "@/interfaces/IProduct";
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { useRouter } from "next/navigation";
 import { CardItem } from "./card-item";
 
 jest.mock("next/navigation", () => ({
-  useRouter: () => ({
-    push: jest.fn(),
-    replace: jest.fn(),
-    back: jest.fn(),
-    forward: jest.fn(),
-    prefetch: jest.fn(),
-    refresh: jest.fn(),
-    pathname: "/mocked-path", // Defina um valor padrão para testes
-  }),
+  useRouter: jest.fn(),
 }));
+
 describe("CardItem", () => {
   const product: IProduct = {
     id: 1,
@@ -31,5 +25,28 @@ describe("CardItem", () => {
   test("Deve ser possivel renderizar um produto", () => {
     render(<CardItem product={product} />);
     expect(screen.getByRole("listitem")).toBeInTheDocument();
+  });
+
+  test("Deve ser possivel clicar em um produto e redirecionar para tela de detalhes", () => {
+    const mockPush = jest.fn();
+
+    (useRouter as jest.Mock).mockReturnValue({
+      push: mockPush,
+    });
+
+    render(<CardItem product={product} />);
+
+    fireEvent.click(screen.getByText(product.title));
+
+    expect(mockPush).toHaveBeenCalledWith(`/${product.id}`);
+  });
+
+  test("Deve ser possivel visualizar o valor do item formatado", () => {
+    render(<CardItem product={product} />);
+
+    const formattedPrice = `Preço: R$ 100,00`;
+    const priceElement = screen.getByText(formattedPrice);
+
+    expect(priceElement).toBeInTheDocument();
   });
 });
